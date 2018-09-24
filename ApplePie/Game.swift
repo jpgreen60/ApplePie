c298ec0d85e780ac9ffd932182585e98f2d6e44c
John Green <jpgreen@Johns-iMac.local>
2018-09-23 21:39:16 -0400

    Part 2 complete

---
+++ ApplePie/Game.swift
@@ -0,0 +1,14 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by John Green on -09-232018.
+//  Copyright © 2018 John P. Green. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var listOfWords = ["receiver", "tv", "speakers", "bluray", "playstation", "appletv", "roku", "wii", "fibereceiver"]
+    let incorrectMovesAllowed = 7
+    var totalWins = 0
+    var totalLosses = 0
+    var currentGame: Game!
+    
+    
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+   
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        newRound()
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
+    func newRound() {
+        
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+        
+    }
+    
+    
 }
 
