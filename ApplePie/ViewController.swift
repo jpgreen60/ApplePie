2072ea50127d157fed0be391358ee25e54df6d7d
John Green <jpgreen@Johns-iMac.local>
2018-09-24 20:26:53 -0400

    Complete - horrible training project with many errors

--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -12,9 +12,18 @@ class ViewController: UIViewController {
     
     var listOfWords = ["receiver", "tv", "speakers", "bluray", "playstation", "appletv", "roku", "wii", "fibereceiver"]
     let incorrectMovesAllowed = 7
-    var totalWins = 0
-    var totalLosses = 0
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
     var currentGame: Game!
+    
     
     
     @IBOutlet weak var treeImageView: UIImageView!
@@ -35,15 +44,47 @@ class ViewController: UIViewController {
         let letterString = sender.title(for: .normal)!
         let letter = Character(letterString.lowercased())
         currentGame.playerGuessed(letter: letter)
-        updateUI()
+        updateGameState()
         
     }
     
     func newRound() {
         
-        let newWord = listOfWords.removeFirst()
-        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
-        updateUI()
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        }
+        
+        else {
+            enableLetterButtons(false)
+        }
+        
+    }
+    
+    func enableLetterButtons(_ enable: Bool) {
+        for button in letterButtons {
+            button.isEnabled = enable
+        }
+    }
+    
+    
+    
+    
+    func updateGameState() {
+        
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        }
+        
+        else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        }
+        
+        else {
+            updateUI()
+        }
         
     }
     
